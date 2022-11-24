9c26c79df246e306d0c40e054d5cbde59b37ae00
sozib62 <webdevsozib62@gmail.com>
2022-11-25 03:10:17 +0600

    added category get method

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-const { config } = require('dotenv');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.we0bjkr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoryCollection = client.db('buySell').collection('category')
+
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id };
+            const result = await categoryCollection.find(query).toArray()
+            res.send(result);
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
 
 app.get('/', async (req, res) => {
     res.send('assignment 12 server is running')
