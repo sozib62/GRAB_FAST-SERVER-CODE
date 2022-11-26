419f2dcbe1c514d8a031bf11f5dad8fb5fa2fc54
sozib62 <webdevsozib62@gmail.com>
2022-11-27 00:29:18 +0600

    added user delete method

--- index.js
+++ index.js
@@ -86,6 +86,13 @@ async function run() {
             res.send(result);
         })
 
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        })
+
         app.get('/users', async (req, res) => {
             const query = {}
             const users = await usersCollection.find(query).toArray();
